4e029f976b21ed407b89f3b94293b87edf58297f
Dmitry Kovalkov <ikekoval@gmail.com>
2019-03-23 19:37:03 +0300

    initial commit

---
+++ test/main.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+// my function
+void swap(int a, int b)
+{
+
+}
+
+void swap(float a, float b)
+{
+
+}
+
+int main()
+{
+	std::cout << "fff" << std::endl;
+	system("pause");
+	int a;
+
+
+
+	a = 1;
+	int f = 2;
+}
+	
